049d0112ad2e4aef19705b624d977cf81b538137
Alexander Steingold <alex461976@gmail.com>
2023-06-27 12:05:35 +0300

    implement custom generic form button functionality

--- frontend/src/app/components/partials/default-button/default-button.component.ts
+++ frontend/src/app/components/partials/default-button/default-button.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
-  selector: 'app-default-button',
+  selector: 'default-button',
   templateUrl: './default-button.component.html',
   styleUrls: ['./default-button.component.css']
 })
-export class DefaultButtonComponent {
+export class DefaultButtonComponent implements OnInit {
+  @Input() type: 'submit' | 'button' = 'submit';
+  @Input() text: string = 'Submit';
+  @Input() bgColor: string = '';
+  @Input() color: string = '';
+  @Input() fontSizeRem: number;
+  @Input() widthRem: number;
+  @Input() styleClass: string;
+  @Output() onClick = new EventEmitter<any>();
+
+  ngOnInit(): void {
+  }
+
 
 }
